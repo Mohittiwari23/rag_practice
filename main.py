cdccc7f617fdd125d3e1107c7be87f4ac24decd8
Mohittiwari23 <inbox.mohittiwari@gmail.com>
2026-05-26 16:06:36 +0530

    initialized

---
+++ main.py
@@ -0,0 +1,9 @@
+# Mohit Tiwari
+# 26/05/2026
+# Description: Practicing implementing RAG framework and setting up local llm support for future project reference
+
+def main():
+    print("Hello World")
+
+if __name__ == '__main__':
+    main()
